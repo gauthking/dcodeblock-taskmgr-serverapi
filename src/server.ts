ee725a263e321a77f191cf5ed5acdc4ca8c7c5bc
Gautham Krishna <90638995+gauthking@users.noreply.github.com>
2024-11-03 10:38:38 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -13,14 +13,14 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://dcodeblock-taskmgr-serverapi.onrender.com",
         methods: ["GET", "POST", "PUT"],
         credentials: true,
     },
 });
 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://dcodeblock-taskmgr-serverapi.onrender.com",
     methods: ["GET", "POST", "PUT"],
     credentials: true,
 }));
